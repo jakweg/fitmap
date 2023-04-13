84c4baa943b56645fd2d4db4d5baeb5266ce2895
Jakub Wegrzyn <jakubek.weg@gmail.com>
2023-04-13 14:36:54 +0200

    Download tcx data and store inside db

---
+++ src/lib/idb.ts
@@ -0,0 +1,24 @@
+export const openIndexedDatabase = (
+	name: string,
+	version: number,
+	upgradeCallback: (result: IDBDatabase) => void,
+) => {
+	return new Promise<IDBDatabase>((resolve, reject) => {
+		const request = indexedDB['open'](name, version)
+		request.onblocked = reject
+		request.onerror = reject
+		request.onsuccess = () => void resolve(request['result'])
+
+		request.onupgradeneeded = () => {
+			const db = request['result']
+			upgradeCallback(db)
+		}
+	})
+}
+
+export const promiseWrapRequest = <T>(request: IDBRequest<T>) => {
+	return new Promise<T>((resolve, reject) => {
+		request.onerror = reject
+		request.onsuccess = () => resolve(request['result'])
+	})
+}
--- src/routes/workouts/+page.svelte
+++ src/routes/workouts/+page.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
+	import { openIndexedDatabase, promiseWrapRequest } from '../../lib/idb'
+
+	const openDb = () =>
+		openIndexedDatabase('fitmap', 1, db => {
+			const store = db.createObjectStore('recordedActivities', { keyPath: 'logId' })
+			store.createIndex('missingTcxData', 'missingTcxData', { unique: false })
+		})
+
 	const makeApiCall = async (
-		options: { fullUrl: string } | { endpoint: string; params: { [key: string]: string } },
+		options: ({ fullUrl: string } | { endpoint: string; params: { [key: string]: string } }) & {
+			asText?: boolean
+		},
 	) => {
 		const accessToken = localStorage.getItem('accessToken')
 		if (!accessToken) throw new Error('Missing access token')
@@ -18,10 +28,12 @@
 			},
 		})
 		if (!response.ok) throw new Error()
+		if (options.asText) return await response.text()
 		return await response.json()
 	}
 
 	const downloadList = async () => {
+		const userId = localStorage.getItem('userId')
 		const allActivities: any[] = []
 		const response = await makeApiCall({
 			endpoint: 'activities/list.json',
@@ -33,27 +45,81 @@
 			},
 		})
 		allActivities.push(...response.activities)
-		let pagination = response.pagination.next
-		while (pagination) {
-			const response = await makeApiCall({ fullUrl: pagination.next })
-			pagination = response.pagination.next
+		let next = response.pagination.next
+		while (next) {
+			const response = await makeApiCall({ fullUrl: next })
+			next = response.pagination.next
 			allActivities.push(...response.activities)
 		}
 
 		const normalizedActivities = allActivities
-			.filter((a) => a.activityTypeId === 90009) // only runs
-			.filter((a) => a.logType !== 'auto_detected') // exclude auto walks
-			.map((a) => ({
+			.filter(a => a.activityTypeId === 90009 || a.activityTypeId === 90001) // only runs or bikes
+			.filter(a => a.logType !== 'auto_detected') // exclude auto walks
+			.filter(a => !!a.tcxLink) // ignore if missing location data
+			.map(a => ({
 				logId: a.logId,
-				tcxLink: 'https://api.fitbit.com/1/user/-/activities/53618063201.tcx',
+				userId,
+				tcxLink: a.tcxLink,
+				missingTcxData: 'T',
 			}))
 
-		console.log(normalizedActivities)
+		const db = await openDb()
+
+		const transaction = db.transaction(['recordedActivities'], 'readwrite')
+		const store = transaction.objectStore('recordedActivities')
+		await Promise.allSettled(
+			normalizedActivities.map(a =>
+				promiseWrapRequest(store.add(a)).catch(e => e.preventDefault()),
+			),
+		)
+
+		db.close()
 	}
 
-	const loadData = () => {
-		downloadList()
+	const downloadTcx = async () => {
+		const userId = localStorage.getItem('userId')
+		const db = await openDb()
+
+		const transaction = db.transaction(['recordedActivities'], 'readonly')
+		const values = (
+			await promiseWrapRequest(
+				transaction
+					.objectStore('recordedActivities')
+					.index('missingTcxData')
+					.getAll(IDBKeyRange.only('T')),
+			)
+		).filter(v => v.userId === userId)
+
+		const parser = new DOMParser()
+
+		await Promise.allSettled(
+			values.map(async v => {
+				const content = await makeApiCall({ fullUrl: v.tcxLink, asText: true })
+				const document = parser.parseFromString(content, 'text/xml')
+				const points = Array.from(document.querySelectorAll('Position'))
+				const positionPoints = new Float64Array(points.length * 2)
+
+				let i = 0
+				for (const position of points) {
+					const lat = +(position.querySelector('LatitudeDegrees')?.textContent ?? 0)
+					const lng = +(position.querySelector('LongitudeDegrees')?.textContent ?? 0)
+					positionPoints[i * 2] = lat
+					positionPoints[i * 2 + 1] = lng
+					i++
+				}
+
+				const transaction = db.transaction('recordedActivities', 'readwrite')
+				await promiseWrapRequest(
+					transaction
+						.objectStore('recordedActivities')
+						.put({ ...v, missingTcxData: 'F', tcxData: positionPoints }),
+				)
+			}),
+		)
+
+		db.close()
 	}
 </script>
 
-<button on:click={loadData}>Download data</button>
+<button on:click={downloadList}>Download activities list</button>
+<button on:click={downloadTcx}>Download tcx data</button>
